5b4f56ad771ddefb4baa1df56b95a8160e47b171
unknown <amarnathsahu78@gmail.com>
2020-05-01 01:01:06 +0530

    added url path

--- src/main/java/com/amar/controller/MasterController.java
+++ src/main/java/com/amar/controller/MasterController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MasterController {
 
-	@GetMapping
+	@GetMapping("/welcome")
 	public String getGreetingMsg() {
 		return "Success";
 	}
