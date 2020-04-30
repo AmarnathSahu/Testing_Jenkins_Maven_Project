6a90d2baf13ae93410f87c8e096e843bed293520
unknown <amarnathsahu78@gmail.com>
2020-05-01 00:39:31 +0530

    project commit to git

---
+++ src/main/java/com/amar/app/RestSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.amar.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestSpringProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amar/app/RestSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amar.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
