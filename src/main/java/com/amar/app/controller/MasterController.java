00516001c534f3a06cc8cb45ab2e25ad944dd5b3
unknown <amarnathsahu78@gmail.com>
2020-05-01 01:05:27 +0530

    changed package name

--- src/main/java/com/amar/app/controller/MasterController.java
+++ src/main/java/com/amar/app/controller/MasterController.java
@@ -1,4 +1,4 @@
-package com.amar.controller;
+package com.amar.app.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
