77674d577426ed6ad7baa6b5205473be1be57a53
unknown <amarnathsahu78@gmail.com>
2020-05-01 16:17:44 +0530

    Added external tomcat functionality

--- src/main/java/com/amar/app/RestSpringProjectApplication.java
+++ src/main/java/com/amar/app/RestSpringProjectApplication.java
@@ -2,12 +2,19 @@ package com.amar.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RestSpringProjectApplication {
+public class RestSpringProjectApplication extends SpringBootServletInitializer  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestSpringProjectApplication.class, args);
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(RestSpringProjectApplication.class);
+    }
 
 }
